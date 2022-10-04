c71879eb76afb867adb4d3a4b0e59fbffea4da34
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-04 19:32:58 +0100

    setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+import http from 'http';
+import Express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app = Express();
+
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+http.createServer(app).listen(() => console.log(`server listening at ${PORT} 🚀`));
